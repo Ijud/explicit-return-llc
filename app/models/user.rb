1f3b7fa4a68d35d84d0854323c09a65a45b5eafc
dylanjshaw <dylanjshaw@sandiego.edu>
2017-03-08 15:39:16 -0800

    created user table, model and controller with new and create actions for registration

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+require 'bcrypt'
+
+class User < ApplicationRecord
+  has_many :cohorts
+
+  validates :username, :email, :encrypted_password, presence: true
+  validates :email, uniqueness: true
+
+  def password
+    @password ||= BCrypt::Password.new(encrypted_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.encrypted_password = @password
+  end
+
+  def authenticate(plain_text_password)
+    self.password == plain_text_password
+  end
+end
