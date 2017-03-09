5abb0e3f9aeb39e18d64bea16555145c2eb0bec4
jacklof <johnloftus026@gmail.com>
2017-03-08 17:56:18 -0800

    Commit randomly

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -2,4 +2,24 @@ class CohortsController < ApplicationController
   def new
     @cohort = Cohort.new
   end
+
+  def show
+    @cohort = Cohort.find(params[:id])
+  end
+
+  def create
+    @cohort = Cohort.new(cohort_params)
+    p @cohort.students
+    # @cohort.user = current_user
+    if @cohort.save
+      redirect_to @cohort
+    else
+      render :new
+    end
+  end
+
+  private
+    def cohort_params
+      params.require(:cohort).permit(:name, :student_data)
+    end
 end
--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -3,4 +3,14 @@ class Cohort < ApplicationRecord
   has_many :students
 
   validates :user, :name, presence: true
+
+  accepts_nested_attributes_for :students
+
+  def student_data
+
+  end
+
+  def student_data=(data)
+    # @students =
+  end
 end
