0e24130e8a63dfb864fca3a65acef667af5d54b0
jacklof <johnloftus026@gmail.com>
2017-03-09 13:10:18 -0800

    Working on display

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -2,17 +2,15 @@ class GroupingsController < ApplicationController
 
   def create
     @cohort = Cohort.find(params[:cohort_id])
+    
     group_up(@cohort.students, params[:groupings][:size].to_i).each_with_index do |group, index|
       group.each do |student|
         @cohort.groupings.new(group_id: index, student: student)
       end
     end
 
-    if @cohort.save
-      # redirect_to
-    else
-      redirect_to @cohort
-    end
+    @cohort.save
+    redirect_to @cohort
   end
 
   def group_up(students, size)
