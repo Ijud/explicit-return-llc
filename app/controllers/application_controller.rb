1a155d278b0317c25d74b401833c1c7eafa2ba81
jacklof <johnloftus026@gmail.com>
2017-03-09 09:48:12 -0800

    Add fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_user, :logged_in?
+
   def current_user
     User.find_by(id: session[:user_id])
   end
@@ -10,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def logout
-    session[:user_id] = nil
+    session.delete(:user_id)
   end
 
   def logged_in?
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      login @user
       redirect_to user_path(@user)
     else
       render 'new'
