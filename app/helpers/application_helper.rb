e13b9965476bbddd3d9197e5e7f1b263e18ad6a3
jacklof <johnloftus026@gmail.com>
2017-03-10 13:31:00 -0800

    Add table-fied markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def groupings_plaintext(cohort, grouping_id)
-    plaintext = ''
+    plaintext = "#{cohort.name}\n\n"
     cohort.groupings.select(:group_id).where(grouping_id: grouping_id).group(:group_id).count.size.times do |grp|
 
       plaintext << "Group #{grp + 1}:\n"
@@ -19,19 +19,16 @@ module ApplicationHelper
   end
 
   def groupings_markdown(cohort, grouping_id)
-    markdown = ''
+    markdown = "# #{cohort.name}\n\n"
+    markdown << "Group | Name\n"
+    markdown << ":---: | :---:\n"
     cohort.groupings.select(:group_id).where(grouping_id: grouping_id).group(:group_id).count.size.times do |grp|
-
-      markdown << "**Group #{grp + 1}:**\n"
 
       cohort.groupings.where(grouping_id: grouping_id, group_id: grp).each do |g|
 
-        markdown << "- #{g.student.name}\n"
+        markdown << "#{grp + 1} | #{g.student.name}\n"
 
       end
-
-      markdown << "\n"
-
     end
     markdown
   end
